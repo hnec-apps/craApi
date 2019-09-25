fa92528601bf481db66604122ae7f034076033b5
abdooageel <ageel.abdoo@gmail.com>
2019-09-25 11:49:57 +0200

    add to tempcitizen

--- controller/citizen.js
+++ controller/citizen.js
@@ -37,7 +37,40 @@ exports.citizen_mgr = {
   },
   add_citizen: async body => {
     try {
-      return await models.Citizen.create(body);
+      let citizen = await models.Citizen.findOne({
+        where: {
+          person_id: body.person_id
+        }
+      });
+      console.log("im hh");
+      if (citizen) {
+        console.log(" im in add", citizen);
+        let tempcitizen = {
+          person_id: citizen.person_id,
+          national_id: citizen.national_id,
+          first_name: citizen.first_name,
+          father_name: citizen.father_name,
+          grand_name: citizen.grand_name,
+          family_name: citizen.family_name,
+          mother_name: citizen.mother_name,
+          birth_date: citizen.birth_date,
+          gender: citizen.gender,
+          is_alive: citizen.is_alive,
+          registry_number: citizen.registry_number,
+          deleted: citizen.deleted
+
+        }
+        await models.tempCitizen.create(tempcitizen);
+        await models.Citizen.destroy({
+          where: {
+            person_id: body.person_id
+          }
+        });
+        return await models.Citizen.create(tempcitizen);
+      } else {
+        return await models.Citizen.create(tempcitizen);
+      }
+
     } catch (err) {
       return (err);
     }
--- modules/citizen.js
+++ modules/citizen.js
@@ -1,21 +1,20 @@
-
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Citizen = sequelize.define('Citizen', {
     person_id: DataTypes.NUMERIC(15),
-    national_id : DataTypes.STRING(20),
-    first_name : DataTypes.STRING(150),
-    father_name : DataTypes.STRING(150),
-    grand_name : DataTypes.STRING(150),
-    family_name : DataTypes.STRING(150),
-    mother_name : DataTypes.STRING(150),
-    birth_date : DataTypes.DATE(),
-    gender : DataTypes.NUMERIC(2),
-    is_alive :DataTypes.STRING(10),
-    registry_number : DataTypes.STRING(10),
-    deleted : DataTypes.NUMERIC(1)
+    national_id: DataTypes.STRING(20),
+    first_name: DataTypes.STRING(150),
+    father_name: DataTypes.STRING(150),
+    grand_name: DataTypes.STRING(150),
+    family_name: DataTypes.STRING(150),
+    mother_name: DataTypes.STRING(150),
+    birth_date: DataTypes.DATE(),
+    gender: DataTypes.NUMERIC(2),
+    is_alive: DataTypes.STRING(10),
+    registry_number: DataTypes.STRING(30),
+    deleted: DataTypes.NUMERIC(1)
   }, {});
-  Citizen.associate = function(models) {
+  Citizen.associate = function (models) {
     // associations can be defined here
   };
   return Citizen;
